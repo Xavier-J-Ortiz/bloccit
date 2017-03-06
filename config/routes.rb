81b8787aaa322945dc154a1231e2aba051f026ca
xortiz <cuchumino@gmail.com>
2017-03-06 09:09:36 -0500

    left these changes, need to integrate everything correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'sponsored_post/show'
-
-  get 'sponsored_post/new'
-
-  get 'sponsored_post/edit'
-
   resources :topics do
     resources :posts, except: [:index]
     resources :sponsored_posts, except: [:index]
