84f66c18ddc143997c31b04b33ce9eff5d754a04
Xavier Ortiz <cuchumino@gmail.com>
2017-03-10 08:58:45 -0500

    added some code with Micheal to get this up and running. stuck on redirect_to portion. Will work through this.

--- Gemfile
+++ Gemfile
@@ -35,5 +35,7 @@ gem 'bootstrap-sass'
 
 gem 'bcrypt'
 
+gem 'pry'
+
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def confirm
+    # binding.pry
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+      redirect_to users_confirm_path
+    else
+      flash.now[:alert] = "There was an error creating your account. Please try again."
+      render :new
+    end
+
+  end
+
   def create
     @user = User.new
     @user.name = params[:user][:name]
