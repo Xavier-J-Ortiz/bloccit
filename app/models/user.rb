c4ffb646018d009673424100a58f14e9a3d67c1c
Xavier Ortiz <cuchumino@gmail.com>
2017-03-13 16:59:40 -0400

    done with assignment. Searching as to why my topics_controller_spec.rb is failing

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ActiveRecord::Base
   before_save { self.role ||= :member }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-  validates :password, length: { minimum: 6 }, allow_blank: true
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 254 }
+  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
   has_secure_password
 
