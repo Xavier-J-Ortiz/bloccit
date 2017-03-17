0e199792f17dd8b8209f84f5c0fc2f28fafd5729
xortiz <cuchumino@gmail.com>
2017-03-16 20:00:04 -0400

    vote model added, user and post updated with vote as well.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
-  has_many :comments, dependent: :destroy
   belongs_to :user
   belongs_to :topic
+  has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
   default_scope { order('created_at DESC') }
   scope :ordered_by_title, -> { order('title') }
