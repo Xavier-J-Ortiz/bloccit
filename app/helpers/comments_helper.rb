6a87b303be250077a14940fbebc3d4edf50f6585
xortiz <cuchumino@gmail.com>
2017-03-20 07:21:02 -0400

    added helper files to comments_helper and posts_helper, and added neat differentiator to view/users/show

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,14 @@ module CommentsHelper
   def user_is_authorized_for_comment?(comment)
     current_user && (current_user == comment.user || current_user.admin?)
   end
+
+  def user_has_comments
+    puts
+
+    if current_user.comments.count == 0
+      return false
+    else
+      return true
+    end
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,4 +4,11 @@ module PostsHelper
     current_user && (current_user == post.user || current_user.admin?)
   end
 
+  def user_has_posts
+    if current_user.posts.count == 0
+      return false
+    else
+      return true
+    end
+  end
 end
